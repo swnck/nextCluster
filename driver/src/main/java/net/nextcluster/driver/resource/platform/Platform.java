7f86582a8cdc5245e5a09d2a86a02a52695686d9
swnck <datapack3t.contact@gmail.com>
2024-03-29 01:56:04 +0100

    Added enum Platform type

--- driver/src/main/java/net/nextcluster/driver/resource/platform/Platform.java
+++ driver/src/main/java/net/nextcluster/driver/resource/platform/Platform.java
@@ -3,14 +3,12 @@ package net.nextcluster.driver.resource.platform;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.Accessors;
-import net.nextcluster.driver.resource.platform.impl.CustomPlatform;
-
 @Getter
 @Accessors(fluent = true)
 @AllArgsConstructor
 public abstract class Platform {
 
-    private String type;
+    private PlatformType type;
     private String id;
 
 }
---
+++ driver/src/main/java/net/nextcluster/driver/resource/platform/PlatformType.java
@@ -0,0 +1,7 @@
+package net.nextcluster.driver.resource.platform;
+
+public enum PlatformType {
+    CUSTOM,
+    SERVER,
+    PROXY
+}
